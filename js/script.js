e5200ce9fff148c90fa5eac016cadb1fef291e0e
akala <113003736+akalalane@users.noreply.github.com>
2022-11-08 21:36:15 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
- function init(){
-   function eventMessageFunction(){
-     var input=document.getElementById('entryinput').value;
-     var output=document.getElementById('textoutput');
-     output.innerHTML=input;
-     alert('Goodwin Akala Lane: '+input);
+function init(){
+  function eventMessageFunction(){
+    var input=document.getElementById('entryinput').value;
+    var output=document.getElementById('textoutput');
+    output.innerHTML=input;
+    alert('Goodwin Akala Lane: '+input);
   }
   var button=document.getElementById('entrybutton');
   button.addEventListener('click', eventMessageFunction);
